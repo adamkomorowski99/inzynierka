c38618dc64d4bd87813ad49e915e87b90556bbca
adamkomorowski99 <adamkomorowski99@gmail.com>
2022-11-16 00:34:39 +0100

    update 16.11.2022
    update 16.11.2022

---
+++ My project/Assets/Projekt/Scripts/SwitchClass.cs
@@ -0,0 +1,14 @@
+using Unity.VisualScripting;
+using UnityEngine;
+ 
+[Inspectable] 
+public class SwitchClass
+{
+    [Inspectable] public bool turnOn;
+    [Inspectable] public bool turnOff;
+    [Inspectable] public string Tag;
+    [Inspectable] public GameObject objectFirst;
+    [Inspectable] public GameObject ObjectSecond;
+
+}
+ 
