072d839a93a7789d3b97d0be3f4cdd055388e338
adamkomorowski99 <adamkomorowski99@gmail.com>
2022-12-19 04:48:59 +0100

    aktualizacja
    19.12.2022

---
+++ AktualizacjaGrudzien19/Assets/Projekt/Scripts/SwitchClass.cs
@@ -0,0 +1,16 @@
+using Unity.VisualScripting;
+using UnityEngine;
+ 
+[Inspectable] 
+public class SwitchClass
+{
+    [Inspectable] public bool turnOn;
+    [Inspectable] public bool turnOff;
+    [Inspectable] public string Tag;
+    [Inspectable] public GameObject objectFirst;
+    [Inspectable] public GameObject ObjectSecond;
+    [Inspectable] public GameObject ObjectThird;
+
+
+}
+ 
