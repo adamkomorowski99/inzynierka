30d94c354cb76ed2938f743f563714e6f12a81f1
adamkomorowski99 <adamkomorowski99@gmail.com>
2023-01-06 00:14:23 +0100

    nujk
    njl

---
+++ styczen/Assets/Projekt/Scripts/SwitchClass.cs
@@ -0,0 +1,13 @@
+using Unity.VisualScripting;
+using UnityEngine;
+ 
+[Inspectable] 
+public class SwitchClass
+{
+    [Inspectable] public bool turnOn;
+    [Inspectable] public string Tag;
+    [Inspectable] public GameObject objectOn;
+    [Inspectable] public float delay;
+
+}
+ 
